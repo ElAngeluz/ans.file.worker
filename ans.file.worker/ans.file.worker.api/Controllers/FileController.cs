590cb0314bea0063b030bd0a4130bdb7a5a78723
gitName <a@b.c>
2023-06-22 23:48:19 -0500

    Se carga proyecto base

---
+++ ans.file.worker/ans.file.worker.api/Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ans.file.worker.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly ILogger<FileController> _logger;
+
+        public FileController(ILogger<FileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetFile")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
