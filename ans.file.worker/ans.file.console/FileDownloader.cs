4eafd337125c790d6520dad3365952ca888b32f9
gitName <a@b.c>
2023-07-01 02:56:39 -0500

    Se carga codigo de descarga de File

---
+++ ans.file.worker/ans.file.console/FileDownloader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ans.file.console
+{
+    internal class FileDownloader
+    {
+        private HttpClient httpClient;
+        private ConcurrentDictionary<string, byte[]> fileCache;
+
+        public FileDownloader()
+        {
+            httpClient = new HttpClient();
+            fileCache = new ConcurrentDictionary<string, byte[]>();
+        }
+
+        public async Task<byte[]?> DownloadFileAsync(string url)
+        {
+            if (fileCache.TryGetValue(url, out byte[]? cachedFile))
+            {
+                return cachedFile;
+            }
+
+            try
+            {
+                byte[] fileBytes = await httpClient.GetByteArrayAsync(url);
+                fileCache.TryAdd(url, fileBytes);
+                return fileBytes;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Error al descargar el archivo: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ ans.file.worker/ans.file.console/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ans.file.console;
+
+string fileUrl = "https://example.com/example.pdf"; // Reemplaza con tu URL de archivo
+
+FileDownloader fileDownloader = new();
+byte[]? fileBytes = await fileDownloader.DownloadFileAsync(fileUrl);
+
+if (fileBytes is not null)
+{
+    // El archivo se ha descargado correctamente
+    // Puedes trabajar con el array de bytes como desees, por ejemplo, guardarlo en disco o enviarlo como respuesta HTTP
+
+    // Ejemplo: Guardar el archivo en disco
+    File.WriteAllBytes("archivo_descargado.pdf", fileBytes);
+
+    // Ejemplo: Enviar el archivo como respuesta HTTP
+    // Aquí necesitarías usar un framework o tecnología específica para enviar el archivo como respuesta HTTP en tu contexto particular
+}
+
+Console.ReadLine();
